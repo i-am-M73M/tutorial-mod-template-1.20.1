8991977de57f830fb51e6fd8ef4c3a8e1cb3a42c
Mark <31747@edu.msa.nl>
2026-02-12 17:53:50 +0100

    Creating the ModItems.java class. and added the Texture files and en_us.json.

--- src/main/java/net/mark/tutorialmod/TutorialMod.java
+++ src/main/java/net/mark/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.mark.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.mark.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,5 +14,6 @@ public class TutorialMod implements ModInitializer {
 	public void onInitialize() {
 
 		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
