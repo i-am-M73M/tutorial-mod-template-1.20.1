c6025344002eb0d636dff213959814123d7f125f
Mark <31747@edu.msa.nl>
2026-02-15 13:58:15 +0100

    made ores drop ruby's.

--- src/main/java/net/mark/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/mark/tutorialmod/block/ModBlocks.java
@@ -6,11 +6,13 @@ import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.mark.tutorialmod.TutorialMod;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block RUBY_BLOCK = registerBlock("ruby_block",
@@ -20,8 +22,16 @@ public class ModBlocks {
             new Block(FabricBlockSettings.copyOf(Blocks.RAW_IRON_BLOCK)));
 
     public static final Block RUBY_ORE = registerBlock("ruby_ore",
-            new Block(FabricBlockSettings.copyOf(Blocks.IRON_ORE)));
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.IRON_ORE).strength(3f), UniformIntProvider.create(2, 4)));
 
+    public static final Block DEEPSLATE_RUBY_ORE = registerBlock("deepslate_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.DEEPSLATE_IRON_ORE).strength(5f), UniformIntProvider.create(4, 6)));
+
+    public static final Block NETHER_RUBY_ORE = registerBlock("nether_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHER_QUARTZ_ORE).strength(2f), UniformIntProvider.create(1, 3)));
+
+    public static final Block END_STONE_RUBY_ORE = registerBlock("end_stone_ruby_ore",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.END_STONE).strength(4f), UniformIntProvider.create(4, 5)));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
